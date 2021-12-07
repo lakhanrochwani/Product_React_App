51ed70aa04dc06ace5f5399ad3a3d99e903570db
lakhanrochwani <rochwanilakhan@gmail.com>
2021-12-07 14:38:29 -0600

    4 files modified

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,17 +1,11 @@
 import { ActionTypes } from '../constants/action-types';
 const initialState = {
-  products: [
-    {
-      id: 1,
-      title: 'Lakhan',
-      category: 'front end engineer',
-    },
-  ],
+  products: [],
 };
 export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case ActionTypes.SELECTED_PRODUCTS:
-      return state;
+    case ActionTypes.SET_PRODUCTS:
+      return { ...state, products: payload };
     default:
       return state;
   }
