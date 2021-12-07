5d40f070d0fb91c1d7924756b9653518f586362c
lakhanrochwani <rochwanilakhan@gmail.com>
2021-12-07 00:27:37 -0600

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './style.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './containers/Header';
-import ProductListing from './containers/ProductListing';
-import ProductDetail from './containers/ProductDetail';
+import ProductListing from './containers/ProductListing.js';
+import ProductDetail from './containers/ProductDetail.js';
+import NotFound from './containers/NotFound.js';
 
 export default function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-        <ProductListing />
-        <ProductDetail />
         <Routes>
           <Route path="/" element={<ProductListing />} />
           <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
