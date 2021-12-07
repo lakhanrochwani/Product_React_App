979be050fe4aa753ced6b685cc8d58db2514f060
lakhanrochwani <rochwanilakhan@gmail.com>
2021-12-07 15:49:25 -0600

    3 files modified

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -6,6 +6,7 @@ import { selectedProduct } from '../redux/actions/productActions';
 
 const ProductDetail = () => {
   const product = useSelector((state) => state.product);
+  const {image, title, price, description, category} = product;
   const { productId } = useParams();
   const dispatch = useDispatch();
   const fetchProductDetail = async () => {
@@ -20,9 +21,37 @@ const ProductDetail = () => {
     if (productId && productId !== '') fetchProductDetail();
   }, [productId]);
   return (
-    <div>
-      <h1>ProductDetail</h1>
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label">${price}</a>
+                </h2>
+                <h3 className="ui brown block header">{category}</h3>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
+  );
   );
 };
 
