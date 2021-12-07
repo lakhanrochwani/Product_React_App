9581441a9940c4be20ffb2cc2fc054f7148949d0
lakhanrochwani <rochwanilakhan@gmail.com>
2021-12-07 00:07:35 -0600

    5 files modified, 2 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Header />
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
